5289fb53b92200ebb2b6b056c0aaf6595979fcf8
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 17:59:28 +0100

    Add fromLocation

--- lib/navigation/app_link.dart
+++ lib/navigation/app_link.dart
@@ -22,7 +22,26 @@ class AppLink {
   });
 
 // TODO: Add fromLocation
+  static AppLink fromLocation(String? location) {
+    // 1
+    location = Uri.decodeFull(location ?? '');
+    // 2
+    final uri = Uri.parse(location);
+    final params = uri.queryParameters;
 
+    // 3
+    final currentTab = int.tryParse(params[AppLink.tabParam] ?? '');
+    // 4
+    final itemId = params[AppLink.idParam];
+    // 5
+    final link = AppLink(
+      location: uri.path,
+      currentTab: currentTab,
+      itemId: itemId,
+    );
+    // 6
+    return link;
+  }
 // TODO: Add toLocation
 
 }
