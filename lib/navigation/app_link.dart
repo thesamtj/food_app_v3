f85925652a66bf2557dbf8d8a7ce34074da52fc4
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 18:02:56 +0100

    Add toLocation

--- lib/navigation/app_link.dart
+++ lib/navigation/app_link.dart
@@ -42,6 +42,42 @@ class AppLink {
     // 6
     return link;
   }
-// TODO: Add toLocation
 
+// TODO: Add toLocation
+  String toLocation() {
+    // 1
+    String addKeyValPair({
+      required String key,
+      String? value,
+    }) =>
+        value == null ? '' : '${key}=$value&';
+    // 2
+    switch (location) {
+      // 3
+      case loginPath:
+        return loginPath;
+      // 4
+      case onboardingPath:
+        return onboardingPath;
+      // 5
+      case profilePath:
+        return profilePath;
+      // 6
+      case itemPath:
+        var loc = '$itemPath?';
+        loc += addKeyValPair(
+          key: idParam,
+          value: itemId,
+        );
+        return Uri.encodeFull(loc);
+      // 7
+      default:
+        var loc = '$homePath?';
+        loc += addKeyValPair(
+          key: tabParam,
+          value: currentTab.toString(),
+        );
+        return Uri.encodeFull(loc);
+    }
+  }
 }
